202ff1b9543a2cd7539ff93d5cd890dfe9c222e9
vignesh032 <kyasaramvignesh03@gmail.com>
2026-06-06 18:58:06 +0530

    app.css->App.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./app.css";
+import "./App.css";
 import Nav from "./components/Nav/Nav";
 import Sidebar from "./components/Sidebar/sidebar";
 import { Route, Routes } from "react-router-dom";
